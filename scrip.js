2798c9c531a210e264250ae557101a71c8fc773f
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-04-16 10:04:29 +0500

    privet pupsik
    privet pupsik

---
+++ scrip.js
@@ -0,0 +1,5 @@
+let backgroundColor = prompt("Enter background color for body:");
+let color = prompt("Enter your text color:");
+
+document.body.style.backgroundColor = backgroundColor;
+document.body.style.color = color;
